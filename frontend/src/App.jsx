60312a0c1ffe77fbdfa79326ae0519544d1ba9a5
Nilesh Kundu <96992736+nileshk12@users.noreply.github.com>
2025-06-24 23:37:59 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    fetch('http://34.216.197.13:3000/books')  // backend URL
+    fetch('http://backend:3000/books')  // backend URL
       .then(res => res.json())
       .then(data => setBooks(data))
       .catch(err => console.error("API error", err));
